e9afee332d9403758f7a593cec17f161a2900496
Lennart Köhler <lenkoe@web.de>
2019-04-27 20:39:00 +0200

    adds asdict method to database

--- NflDataLoader/player_db.py
+++ NflDataLoader/player_db.py
@@ -1,10 +1,11 @@
-from datetime import date
-
+# from datetime import date
 import sqlalchemy as db
 from sqlalchemy import Column, Integer, String, Date, or_
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import MultipleResultsFound
+
+import pandas as pd
 
 
 Base = declarative_base()
@@ -25,8 +26,28 @@ class Player(Base):
     team = Column(String)
     esb_id = Column(String)
 
+
     def __repr__(self):
         return f"Player: {self.id}, {self.name}, {self.status}"
+
+
+    def asdict(self):
+        return {
+            'id': self.id,
+            'player_id': self.player_id,
+            'trikotnumber': self.trikotnumber,
+            'name': self.name,
+            'position': self.position,
+            'status': self.status,
+            'height': self.height,
+            'weight': self.weight,
+            'birthdate': self.birthdate,
+            'age': self.age,
+            'exp': self.exp,
+            'college': self.college,
+            'team': self.team,
+            'esb_id': self.esb_id
+        }
 
 
 class Players():
@@ -87,36 +108,23 @@ class Players():
 
 
     def get_first_player(self):
-        p = self.session.query(Player).first()
-        return p
+        first = self.session.query(Player).first()
+        return first.asdict()
 
 
     def get_active_players(self):
-        active_players = self.session.query(Player).filter_by(status='ACT')
-        print(active_players.all())
-        return "active"
+        return [player.asdict() for player in self.session.query(Player).filter_by(status='ACT')]
+        
 
 
-if __name__ == "__main__":    
+if __name__ == "__main__":
     dbase = Players(echo=False)
-    new = dbase.create_player({'name': "Test Player", 'player_id': 1,
-                               'position': 'WR', 'trikotnumber': 1, 'age': 20,
-                               'birthdate': date.today(), 'status':'ACT'})
-    dbase.add_player(new)
-    new2 = dbase.create_player({'name': "Inactive", "status":'', 'player_id': 2})
-    dbase.add_player(new2)
-    new3 = dbase.create_player({'name': "Active Player", "player_id": 3, "status": 'ACT'})
-    dbase.add_player(new3)
-
-    q = dbase.get_first_player()
-    print(q)
-    active = dbase.get_active_players()
-
-
-'''
-add multiple objects to the database
-session.add_all([
-     User(name='wendy', fullname='Wendy Williams', nickname='windy'),
-     User(name='mary', fullname='Mary Contrary', nickname='mary'),
-     User(name='fred', fullname='Fred Flintstone', nickname='freddy')])
-'''
+    q = dbase.get_active_players()
+    df = pd.DataFrame(q)
+    print(df.head())
+    # active = dbase.get_active_players()
+# add multiple objects to the database
+# session.add_all([
+#      User(name='wendy', fullname='Wendy Williams', nickname='windy'),
+#      User(name='mary', fullname='Mary Contrary', nickname='mary'),
+#      User(name='fred', fullname='Fred Flintstone', nickname='freddy')])
